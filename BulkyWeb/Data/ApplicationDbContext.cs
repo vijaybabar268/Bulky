7e66edfb3a1a1b9c4aa4a112f050b1fdc6849b80
vijaybabar <babarvm@gmail.com>
2023-12-30 20:39:58 +0530

    Section 2 Seed categories data

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -11,5 +11,17 @@ namespace BulkyWeb.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        // Using method to Seed data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var categoriesList = new List<Category>()
+            {
+                new Category() { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category() { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category() { Id = 3, Name = "History", DisplayOrder = 3 }
+            };
+            modelBuilder.Entity<Category>().HasData(categoriesList);
+        }
     }
 }
