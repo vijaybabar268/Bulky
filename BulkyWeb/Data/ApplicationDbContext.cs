97a2f9905db0fdef0551b5f336fa43d3b3dc21e1
vijaybabar <babarvm@gmail.com>
2023-12-30 20:11:35 +0530

    Section 2 Database created using update-database cmd via package manager console

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {            
+        }
+    }
+}
