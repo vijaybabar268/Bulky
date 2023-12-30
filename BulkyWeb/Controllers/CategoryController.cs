d14a8e679f8081568fb8bc285697017371bc163f
vijaybabar <babarvm@gmail.com>
2023-12-30 21:51:27 +0530

    Section 2 Get all categoris and display in view

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var categories = _db.Categories.OrderBy(c => c.DisplayOrder);
+            return View(categories);
         }
     }
 }
