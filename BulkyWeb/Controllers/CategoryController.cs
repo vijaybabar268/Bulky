7a06b4bc87d137565a2e2473680a7c7a7ae48b7e
vijaybabar <babarvm@gmail.com>
2023-12-31 13:02:44 +0530

    End of the Section 2Perforem CRUD operations around category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
@@ -14,8 +15,65 @@ namespace BulkyWeb.Controllers
 
         public IActionResult Index()
         {
-            var categories = _db.Categories.OrderBy(c => c.DisplayOrder);
+            var categories = _db.Categories.OrderByDescending(c => c.DisplayOrder);
             return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category model)
+        {
+            if (model.Name == model.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("displayOrder", "The DisplayOrder cannot exactly math the name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(model);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(model);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            return View(category);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int id)
+        {
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
         }
     }
 }
